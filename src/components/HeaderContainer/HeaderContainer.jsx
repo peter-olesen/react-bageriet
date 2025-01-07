6bdd3bf8924a9cf745c3f44abfa88a355ffeab2f
Peter Olesen <63056450+peter-olesen@users.noreply.github.com>
2025-01-07 09:13:38 +0100

    minor changes and update to vite@6.0.7

--- src/components/HeaderContainer/HeaderContainer.jsx
+++ src/components/HeaderContainer/HeaderContainer.jsx
@@ -1,5 +1,10 @@
 import hs from "./HeaderContainer.module.scss";
 
 export const HeaderContainer = ({ children }) => {
-  return <header className={hs.Header}>{children}</header>;
+  return (
+    <header className={hs.Header}>
+      {children}
+      <h2>Vi elsker at lave brød</h2>
+    </header>
+  );
 };
