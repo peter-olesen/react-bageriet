ad49cbb0cb6ba17ee47ad41dc33ba01441f2c0b8
Peter Olesen <63056450+peter-olesen@users.noreply.github.com>
2025-01-08 14:11:44 +0100

    something

--- src/components/ProductCardSection/ProductCardSection.jsx
+++ src/components/ProductCardSection/ProductCardSection.jsx
@@ -1,8 +1,43 @@
+import { useState, useEffect } from "react";
 import pcs from "./ProductCardSection.module.scss";
-import ProductImg from "../../assets/img/produkter/1.png";
 import { ProductCard } from "../ProductCard/ProductCard";
 
 export const ProductCardSection = () => {
+  const [products, setProducts] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch(
+          "https://api.mediehuset.net/bakeonline/products"
+        );
+        if (!res.ok) {
+          throw new Error("Could not fetch products");
+        }
+
+        const data = await res.json();
+
+        const mixedProducts = data.items
+          ? data.items.sort(() => Math.random() - 0.5).slice(0, 8)
+          : [];
+        setProducts(mixedProducts);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
+  const truncateText = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return text.slice(0, maxLength) + "...";
+    }
+    return text;
+  };
+
   return (
     <section className={pcs.ProductCardSection}>
       <h2>Nyeste bagværk</h2>
@@ -12,46 +47,17 @@ export const ProductCardSection = () => {
         tilfældige ord, som på ingen måde ser ægte ud
       </p>
       <div className={pcs.ProductGrid}>
-        <ProductCard
-          product="Håndværker"
-          image={ProductImg}
-          text="Der er mange tilgængelige udgaver af Lorem Ipsum, men de fleste udgaver har gennemgået..."
-        />
-        <ProductCard
-          product="Håndværker"
-          image={ProductImg}
-          text="Der er mange tilgængelige udgaver af Lorem Ipsum, men de fleste udgaver har gennemgået..."
-        />
-        <ProductCard
-          product="Håndværker"
-          image={ProductImg}
-          text="Der er mange tilgængelige udgaver af Lorem Ipsum, men de fleste udgaver har gennemgået..."
-        />
-        <ProductCard
-          product="Håndværker"
-          image={ProductImg}
-          text="Der er mange tilgængelige udgaver af Lorem Ipsum, men de fleste udgaver har gennemgået..."
-        />
-        <ProductCard
-          product="Håndværker"
-          image={ProductImg}
-          text="Der er mange tilgængelige udgaver af Lorem Ipsum, men de fleste udgaver har gennemgået..."
-        />
-        <ProductCard
-          product="Håndværker"
-          image={ProductImg}
-          text="Der er mange tilgængelige udgaver af Lorem Ipsum, men de fleste udgaver har gennemgået..."
-        />
-        <ProductCard
-          product="Håndværker"
-          image={ProductImg}
-          text="Der er mange tilgængelige udgaver af Lorem Ipsum, men de fleste udgaver har gennemgået..."
-        />
-        <ProductCard
-          product="Håndværker"
-          image={ProductImg}
-          text="Der er mange tilgængelige udgaver af Lorem Ipsum, men de fleste udgaver har gennemgået..."
-        />
+        {products?.map((item) => {
+          return (
+            <ProductCard
+              key={item.title}
+              product={item.title}
+              comments={item.num_comments}
+              image={item.image.fullpath}
+              text={truncateText(item.teaser, 120)}
+            />
+          );
+        })}
       </div>
     </section>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,35 @@
+import ls from "../style/pages/Login.module.scss";
+import { SectionHeader } from "../components/SectionHeader/SectionHeader";
+
 export const Login = () => {
   return (
     <>
-      <h2>Login</h2>
-      <p>Just a React and Vite boilerplate.</p>
+      <SectionHeader>
+        <h2>Login</h2>
+        <p>
+          Log ind på bageriet. Har du ikke en bruger, så send os en mail på
+          mail@bageriet.domain
+        </p>
+      </SectionHeader>
+      <div className={ls.Login}>
+        <form>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Din e-mail...."
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Dit password...."
+          />
+          <span>
+            <button>LOGIN</button>
+          </span>
+        </form>
+      </div>
     </>
   );
 };
